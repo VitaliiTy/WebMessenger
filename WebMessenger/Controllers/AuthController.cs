18bed8cafeec111c7d5c07a6b1d13f3ab643dc44
Vitalii Baryliak <vitalik27578@gmail.com>
2023-03-23 00:46:17 +0200

    Verify branch protection commit

--- WebMessenger/Controllers/AuthController.cs
+++ WebMessenger/Controllers/AuthController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Runtime.Intrinsics.X86;
 using System.Security.Claims;
 using WebMessenger.Data;
 using WebMessenger.Models;
